e22dd50320cdae2f7c14031900e3b1f1a0ce8e29
Zunlin Zhang <larryzzl@gmail.com>
2017-03-29 14:25:26 +0800

    debug log

--- main_server.js
+++ main_server.js
@@ -1,26 +1,17 @@
 var http = require("http");
 var url = require("url");
 
-var log4js = require("log4js");
-log4js.configure({
-	appenders: [
-	{type: 'console'},
-	{type: 'file', filename: 'public/main.log', category: 'main'}
-	]
-});
-var logger = log4js.getLogger('main');
-
 function start(route, handle) {
 	function onRequest(request, response) {
 
 		var pathname = url.parse(request.url).pathname;
-		console.log("Request for " + pathname + " received.");
+		//logger.trace("Request for " + pathname + " received.");
 
 		route(handle, pathname, response, request);
 	}
 
 	http.createServer(onRequest).listen(8080);
-	console.log("Server has started");
+	//logger.trace("Server has started");
 }
 
 exports.start = start;
